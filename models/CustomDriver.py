6cc9291883e317db7516f348c2cd5affa7d148d3
Daniel <danielberinger95@gmail.com>
2023-08-19 21:13:33 +0200

    Add CustomDriver.py and Selenium to requirements

---
+++ models/CustomDriver.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+class CustomDriver(webdriver.Chrome):
+    def __init__(self, win_username: str, chrome_profile: str, *args, **kwargs):
+        self.win_username = win_username
+        self.profile_name = chrome_profile
+        self.options = self.get_options()
+        self.service = Service()
+        super(self.__class__, self).__init__(options=self.options, service=self.service, *args, **kwargs)
+        self.maximize_window()
+
+    def get_options(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument(
+            fr"--user-data-dir=C:\Users\{self.win_username}\AppData\Local\Google\Chrome\User Data")
+        options.add_argument(fr'--profile-directory={self.profile_name}')
+
+        return options
