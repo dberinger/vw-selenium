1292cac3e11365da2b8ad0d41035e1afdbccf37f
Daniel <danielberinger95@gmail.com>
2023-08-20 18:21:05 +0200

    Add dev mode to CustomDriver to run on port 9222 chrome.exe

--- models/CustomDriver.py
+++ models/CustomDriver.py
@@ -4,18 +4,24 @@ from selenium.webdriver.chrome.options import Options
 
 
 class CustomDriver(webdriver.Chrome):
-    def __init__(self, win_username: str, chrome_profile: str, *args, **kwargs):
+    def __init__(self, win_username: str, chrome_profile: str, dev_mode: bool, *args, **kwargs):
         self.win_username = win_username
         self.profile_name = chrome_profile
+        self.dev_mode = dev_mode
         self.options = self.get_options()
         self.service = Service()
         super(self.__class__, self).__init__(options=self.options, service=self.service, *args, **kwargs)
-        self.maximize_window()
+        if not self.dev_mode:
+            self.maximize_window()
 
     def get_options(self):
         options = webdriver.ChromeOptions()
-        options.add_argument(
-            fr"--user-data-dir=C:\Users\{self.win_username}\AppData\Local\Google\Chrome\User Data")
-        options.add_argument(fr'--profile-directory={self.profile_name}')
+        if self.dev_mode:
+            options.add_experimental_option("debuggerAddress", "localhost:9222")
+            print("Running in dev mode. Make sure to launch Chrome on port 9222.")
+        else:
+            options.add_argument(
+                fr"--user-data-dir=C:\Users\{self.win_username}\AppData\Local\Google\Chrome\User Data")
+            options.add_argument(fr'--profile-directory={self.profile_name}')
 
         return options
